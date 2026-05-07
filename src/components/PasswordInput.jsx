179e02fe180f79b1df55985c78fe7e89570eda3e
Zane <zimedua@gmail.com>
2026-05-06 19:02:50 -0500

    lab events

--- src/components/PasswordInput.jsx
+++ src/components/PasswordInput.jsx
@@ -1,9 +1,23 @@
 // Code PasswordInput Component Here
-
+import React, { useState } from 'react';
 function PasswordInput (){
+
+const[password, setPassword] = useState('');
+const handleChange = (event) => {
+    setPassword(event.target.value);
+    console.log("Entering password...");
+
+};
     return(
-        <></>
-    )
+    
+    <input
+        type ="password"
+        value={password}
+        onChange={handleChange}
+        placeholder="Enter your password"
+        />
+ 
+    );
 }
 
 export default PasswordInput;
--- src/components/SubmitButton.jsx
+++ src/components/SubmitButton.jsx
@@ -1,9 +1,21 @@
 // Code SubmitButton Component Here
+import React from "react";
 
 function SubmitButton (){
+const handleEnter = () => {
+    console.log("Mouse Entering");
+};
+const handleleave = () => {
+    console.log("Mouse Exiting")
+};
     return(
-        <></>
-    )
+        <button
+        onMouseEnter={handleEnter}
+        onMouseLeave={handleleave}
+        >
+        Submit Password 
+        </button>
+    );
 }
 
 export default SubmitButton;
